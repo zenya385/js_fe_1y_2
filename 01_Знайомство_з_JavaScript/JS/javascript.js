3e2990b1131e363ed7f539af7a2f47f8c2b23ad1
Zeno Roman <zeno385@gmail.com>
2022-12-15 21:16:27 +0200

    add lesson 02

--- 01_Знайомство_з_JavaScript/JS/javascript.js
+++ 01_Знайомство_з_JavaScript/JS/javascript.js
@@ -1,5 +1,3 @@
-"use strict";
-
 a = 3;
 const b = "hello word".toUpperCase();
 console.log("довжина слів >", b);
---
+++ 02_Змінні і типи/js/01-vars.js
@@ -0,0 +1,33 @@
+const UPPER_SNAKE_CASE = 69;
+
+// Константа, що зберігає значення кольору
+const COLOR_TEAL = "#009688";
+
+// Константа, що зберігає повідомлення про результат логіну
+const LOGIN_SUCCESS_MESSAGE = "Ласкаво просимо!";
+
+const age = 10;
+const totalPrice = "368.89";
+const userName = "Fagot";
+const message = "Hello word!";
+const isActive = true;
+const shouldConfirm = false;
+
+const numToString = Number.parseInt(totalPrice);
+console.log(numToString);
+
+console.log(Number.parseInt("15.25pxftjshdx"));
+
+console.log("age->", typeof age);
+console.log("totalPrice->", typeof totalPrice);
+console.log("userName->", typeof userName);
+console.log("message->", typeof message);
+console.log("isActive->", typeof isActive);
+
+console.log("shouldConfirm->", typeof shouldConfirm);
+// console.log("count->", count);
+
+let count = "123";
+count = 300;
+
+console.log("count-let", typeof count);
