38ea0c6ba88962f877297b1e81e982ee5b582ad4
Zeno Roman <zeno385@gmail.com>
2023-01-16 20:03:08 +0200

    add

--- 09_Цикли/js/for.js
+++ 09_Цикли/js/for.js
@@ -108,9 +108,8 @@
 // const number = 10;
 
 // for (let i = 0; i <= number; i += 1) {
-//   if (i % 2 !== 0) {
+//   if (i % 2 === 0) {
 //     continue;
 //   }
-
-//   console.log("парне i: ", i); // 1, 3, 5, 7, 9
+//   console.log("не парне i: ", i); // 1, 3, 5, 7, 9
 // }
