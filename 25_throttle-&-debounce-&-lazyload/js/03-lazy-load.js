21084915f1820b175ec64a51ff4c1c12c7382ac1
Zeno Roman <zeno385@gmail.com>
2023-03-03 11:47:53 +0200

    add lesson 25

---
+++ 25_throttle-&-debounce-&-lazyload/js/03-lazy-load.js
@@ -0,0 +1,15 @@
+/*
+ * Відкладене завантаження зображень (lazy-loading) (концепция)
+
+ */
+
+const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+lazyImages.forEach((image) => {
+  image.addEventListener("load", onImageLoaded, { once: true });
+});
+
+function onImageLoaded(evt) {
+  console.log("Картинка загрузилась");
+  evt.target.classList.add('appear');
+}
