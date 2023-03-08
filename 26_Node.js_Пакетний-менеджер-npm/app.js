a8cbd86bb6f14f724ef00d97a0f6b9ed20018da2
Zeno Roman <zeno385@gmail.com>
2023-03-08 09:15:37 +0200

    add node.js

--- 26_Node.js_Пакетний-менеджер-npm/app.js
+++ 26_Node.js_Пакетний-менеджер-npm/app.js
@@ -14,3 +14,21 @@
 // });
 
 
+// console.log('Hello Node.js')
+
+
+const validator = require("validator");
+
+const validateEmail = (email) => {
+  return validator.isEmail(email);
+};
+
+console.log(
+  "Is mango@mail.com a valid email?: ",
+  validateEmail("mango@mail.com")
+);
+
+console.log(
+  "Is Mangozedog.com a valid email?: ",
+  validateEmail("Mangozedog.com")
+);
